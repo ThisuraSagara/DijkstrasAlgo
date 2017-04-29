5a13bbf826078704c01c230d7024916208cb73fd
Thisura Sagara <thisuras4@gmail.com>
2017-04-30 01:41:18 +0530

    Add files via upload

---
+++ src/Chebyshev.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Thisura Sagara
+ */
+public class Chebyshev {
+     //for Chebyshev Distances,
+    private final double hrVerticalDistance = 1.0;
+    private final double dglDistance = 1.0;
+
+    public Chebyshev() {
+    }
+
+    /**
+     * @return the hrVerticalDistance
+     */
+    public double getHrVerticalDistance() {
+        return hrVerticalDistance;
+    }
+
+   
+    /**
+     * @return the dglDistance
+     */
+    public double getDglDistance() {
+        return dglDistance;
+    }
+
+    
+    
+    
+}
